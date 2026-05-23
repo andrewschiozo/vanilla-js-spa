371ea3ee81956fdc4f131a646efe7305d9f8c76a
Andrews <andrewschiozo@gmail.com>
2026-05-23 19:50:29 -0300

    feat: add routes por arquivo de configuração

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { container } from "@/core/container.js";
+import { Router } from "./core/router.js";
+import { routes } from "./config/routes.js";
 
 const appElement = document.getElementById("app");
 
 async function initApp() {
-    console.log("vanilla js");
-    const storage = await container.resolve("StorageAdapter");
-    console.log(storage);
+    const authService = await container.resolve("AuthService");
+
+    window.router = new Router(routes, appElement, authService);
+    window.router.render();
 }
 
 document.addEventListener("DOMContentLoaded", initApp);
