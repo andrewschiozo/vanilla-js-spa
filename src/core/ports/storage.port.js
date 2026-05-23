3f512f9956c0227078f9ada958350d24bed2298e
Andrews <andrewschiozo@gmail.com>
2026-05-23 19:24:30 -0300

    feat: interface p/ o storage

---
+++ src/core/ports/storage.port.js
@@ -0,0 +1,14 @@
+export class StoragePort {
+    get(key) {
+        throw new Error("Método não implementado");
+    }
+    set(key, value) {
+        throw new Error("Método não implementado");
+    }
+    delete(key) {
+        throw new Error("Método não implementado");
+    }
+    subscribe(key, listener) {
+        throw new Error("Método não implementado");
+    }
+}
